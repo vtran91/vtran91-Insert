eca05a97f844d6f0ac933dd2f3d0422d6f40d217
vtran91 <vtran16@bellsouth.net>
2023-02-21 03:46:55 +0000

    Completed insert record

---
+++ problem1.sql
@@ -0,0 +1,3 @@
+INSERT INTO song (title, artist) VALUES ('Let it be', 'Beatles');
+INSERT INTO song (title, artist) VALUES ('Hotel California', 'Eagles');
+INSERT INTO song (title, artist) VALUES ('Kashmir', 'Led Zeppelin');
